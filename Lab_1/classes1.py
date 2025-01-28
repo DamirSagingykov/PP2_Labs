fde7f90d9bba8294b010a09b56a790c7fc44d7a2
DamirSagingykov <damirsm05@gmail.com>
2025-01-28 14:46:59 +0500

    First commit

---
+++ Lab_1/classes1.py
@@ -0,0 +1,13 @@
+'''class MyStr:
+    def __init__(self):
+        self.string = ""
+
+    def getString(self):
+        self.string = input("your string: ")
+
+    def printString(self):
+        print("your string in uppercase: ", self.string.upper())
+'''
+
+print("Damir 777")
+
