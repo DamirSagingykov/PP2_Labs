ac9318c090bc92808aef159ae9ab1aa4204102af
DamirSagingykov <damirsm05@gmail.com>
2025-02-17 10:52:03 +0500

    Your commit message

--- Lab_3/functions2.py
+++ Lab_3/functions2.py
@@ -1,130 +1,106 @@
-# Dictionary of movies
-
-movies = [
 {
-"name": "Usual Suspects", 
-"imdb": 7.0,
-"category": "Thriller"
-},
-{
-"name": "Hitman",
-"imdb": 6.3,
-"category": "Action"
-},
-{
-"name": "Dark Knight",
-"imdb": 9.0,
-"category": "Adventure"
-},
-{
-"name": "The Help",
-"imdb": 8.0,
-"category": "Drama"
-},
-{
-"name": "The Choice",
-"imdb": 6.2,
-"category": "Romance"
-},
-{
-"name": "Colonia",
-"imdb": 7.4,
-"category": "Romance"
-},
-{
-"name": "Love",
-"imdb": 6.0,
-"category": "Romance"
-},
-{
-"name": "Bride Wars",
-"imdb": 5.4,
-"category": "Romance"
-},
-{
-"name": "AlphaJet",
-"imdb": 3.2,
-"category": "War"
-},
-{
-"name": "Ringing Crime",
-"imdb": 4.0,
-"category": "Crime"
-},
-{
-"name": "Joking muck",
-"imdb": 7.2,
-"category": "Comedy"
-},
-{
-"name": "What is the name",
-"imdb": 9.2,
-"category": "Suspense"
-},
-{
-"name": "Detective",
-"imdb": 7.0,
-"category": "Suspense"
-},
-{
-"name": "Exam",
-"imdb": 4.2,
-"category": "Thriller"
-},
-{
-"name": "We Two",
-"imdb": 7.2,
-"category": "Romance"
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Dictionary of movies\n",
+    "\n",
+    "movies = [\n",
+    "{\n",
+    "\"name\": \"Usual Suspects\", \n",
+    "\"imdb\": 7.0,\n",
+    "\"category\": \"Thriller\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Hitman\",\n",
+    "\"imdb\": 6.3,\n",
+    "\"category\": \"Action\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Dark Knight\",\n",
+    "\"imdb\": 9.0,\n",
+    "\"category\": \"Adventure\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"The Help\",\n",
+    "\"imdb\": 8.0,\n",
+    "\"category\": \"Drama\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"The Choice\",\n",
+    "\"imdb\": 6.2,\n",
+    "\"category\": \"Romance\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Colonia\",\n",
+    "\"imdb\": 7.4,\n",
+    "\"category\": \"Romance\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Love\",\n",
+    "\"imdb\": 6.0,\n",
+    "\"category\": \"Romance\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Bride Wars\",\n",
+    "\"imdb\": 5.4,\n",
+    "\"category\": \"Romance\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"AlphaJet\",\n",
+    "\"imdb\": 3.2,\n",
+    "\"category\": \"War\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Ringing Crime\",\n",
+    "\"imdb\": 4.0,\n",
+    "\"category\": \"Crime\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Joking muck\",\n",
+    "\"imdb\": 7.2,\n",
+    "\"category\": \"Comedy\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"What is the name\",\n",
+    "\"imdb\": 9.2,\n",
+    "\"category\": \"Suspense\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Detective\",\n",
+    "\"imdb\": 7.0,\n",
+    "\"category\": \"Suspense\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"Exam\",\n",
+    "\"imdb\": 4.2,\n",
+    "\"category\": \"Thriller\"\n",
+    "},\n",
+    "{\n",
+    "\"name\": \"We Two\",\n",
+    "\"imdb\": 7.2,\n",
+    "\"category\": \"Romance\"\n",
+    "}\n",
+    "]\n",
+    "\n",
+    "#1.task"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.12.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
 }
-]
-
-#1.
-def is_highly_rated(movie):
-    return movie["imdb"] > 5.5
-print(is_highly_rated(movies[0]))
-
-# 2.
-
-def highly_rated_movies(movies):
-    return [movie for movie in movies if movie["imdb"] > 5.5]
-filtered_movies = highly_rated_movies(movies)
-print(filtered_movies)
-
-# 3.
-
-def movies_by_category(movies, category):
-    return [movie for movie in movies if movie["category"].lower() == category.lower()]
-
-category_name = "Romance"
-filtered_movies = movies_by_category(movies, category_name)
-print(filtered_movies)
-
-
-#4. 
-def average_imdb_score(movies):
-    if not movies:  
-        return 0
-    total_score = sum(movie["imdb"] for movie in movies)
-    return total_score / len(movies)
-print(average_imdb_score(movies)) 
-#5.
-def average_imdb_by_category(movies, category):
-    filtered_movies = []
-    
-    for movie in movies:
-        if movie["category"].lower() == category.lower():
-            filtered_movies.append(movie)
-
-    if len(filtered_movies) == 0:
-        return 0
-    
-    total_score = 0
-    for movie in filtered_movies:
-        total_score += movie["imdb"]
-    
-    return total_score / len(filtered_movies)
-
-category_name = "Romance"
-print(average_imdb_by_category(movies, category_name))  
-
-
