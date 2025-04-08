71d513e8a849d1993975ca7929cb10df6b84a0e5
DamirSagingykov <damirsm05@gmail.com>
2025-04-08 13:49:58 +0500

    second upload

--- Lab_8/snake/snake.py
+++ Lab_8/snake/snake.py
@@ -7,7 +7,7 @@ pygame.init()
 pygame.mixer.init()
 
 # Load background music
-pygame.mixer.music.load('./lab_8/snake/song/Still_Woozy_-_Goodie_Bag_73876176.mp3')
+pygame.mixer.music.load('./lab_8/snake/song/TGC_Dreamers_Embody_Remix.mp3')
 pygame.mixer.music.play(-1)
 
 # Game window size
